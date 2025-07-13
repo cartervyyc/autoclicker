d677d22ccdeef848de9fc99e8e4c38a63a78d54a
cartervyyc <cartervyyc@gmail.com>
2025-07-13 14:54:48 -0600

    Initial Commit

--- autoclicker.py
+++ autoclicker.py
@@ -10,15 +10,19 @@ while True:
     key_to_press = random.randint(0, 3)
     if key_to_press == 0:
         keyboard.press('a')
+        time.sleep(2)
         keyboard.release('a')
     elif key_to_press == 1:
         keyboard.press('s')
+        time.sleep(2)
         keyboard.release('s')
     elif key_to_press == 2:
         keyboard.press('w')
+        time.sleep(2)
         keyboard.release('w')
     elif key_to_press == 3:
         keyboard.press('d')
+        time.sleep(2)
         keyboard.release('d')
 
     time.sleep(2)
